6a05b06a5372fa11ea6ca356974d6e29f0deb05d
Bryan-Wriggler <bryan.h.940707@gmail.com>
2022-06-16 20:11:02 -0700

    Add timer and timertask, so it'll keep delaying the print UI input until it is true

--- 2DKinematicsSummer/src/ClientProgram.java
+++ 2DKinematicsSummer/src/ClientProgram.java
@@ -1,36 +1,47 @@
 import java.util.Timer;
 import java.util.TimerTask;
 
+import junit.framework.Test;
+
 public class ClientProgram {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		KinematicsUI uI = new KinematicsUI();
 		uI.buildUI();
 		
 		//control the time (let the get data delay until it finally has data
+		ClientProgram test = new ClientProgram(); //the object that need to test (from internet)
+		
 		Timer timer = new Timer();
 		boolean hasData = false;
 		
-		/*//fail
 		TimerTask task = new TimerTask() {
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
 				System.out.println("delay task");
+				
+				if (uI.getDataState()) {
+					
+					synchronized(test) { //IDK, but try it
+						test.notify();
+					}
+				}
 			}
 			
 		};
 		
-		while (!hasData) { //before it has data, it'll keep doing the task
-			timer.schedule(task, 1000); //delay 1000 ms
-			hasData = uI.getDataState(); //update it
-			
-			//try to cancel the scheduled task?
-			timer.cancel();
+		//schedule the task, and wait till ui has true data
+		timer.schedule(task, 3000, 3000);
+		
+		synchronized(test) {
+			test.wait();
 		}
-		*/
 		//re
 		
 		System.out.println(uI.getInputs());
+		System.out.println(task.cancel()); //sea if the task will cancel after it
+		
+		timer.cancel(); //cancel the timer
 	}
 }
