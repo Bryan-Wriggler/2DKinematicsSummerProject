d047ffb09c792a67d1db04367cc3f5536f660342
Bryan-Wriggler <bryan.h.940707@gmail.com>
2022-06-18 14:30:35 +0800

    Add notes & clean up the not needed parts inside ClientProgram

--- 2DKinematicsSummer/src/ClientProgram.java
+++ 2DKinematicsSummer/src/ClientProgram.java
@@ -10,20 +10,19 @@ public class ClientProgram {
 		uI.buildUI();
 		
 		//control the time (let the get data delay until it finally has data
-		ClientProgram test = new ClientProgram(); //the object that need to test (from internet)
+		ClientProgram test = new ClientProgram(); //the object that need to test (synchronize)
 		
-		Timer timer = new Timer();
-		boolean hasData = false;
+		Timer timer = new Timer(); //timer
 		
-		TimerTask task = new TimerTask() {
+		TimerTask task = new TimerTask() { //anonymous class of TimerTask
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
 				System.out.println("delay task");
 				
-				if (uI.getDataState()) {
+				if (uI.getDataState()) { //the condition to let it stop
 					
-					synchronized(test) { //IDK, but try it
+					synchronized(test) { //IDK, but try it  (maybe it'll send a signal to clientprogram,  and tell it that it's able to end
 						test.notify();
 					}
 				}
@@ -34,7 +33,7 @@ public class ClientProgram {
 		//schedule the task, and wait till ui has true data
 		timer.schedule(task, 3000, 3000);
 		
-		synchronized(test) {
+		synchronized(test) { //wait until the result was fulfilled (the condition is true)
 			test.wait();
 		}
 		//re
